fcd12dc9f08ce3166d304e1bf5f5bfbfe37a14e8
Adam Cabezas <adamcabezas1218@outlook.com>
2025-06-30 20:19:13 -0700

    Create Buyer.java

---
+++ javafxproject/src/application/models/Buyer.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class Buyer extends User {
+
+    private ArrayList<Book> cart;
+
+    public ArrayList<Book> filterBooks(String[] condition, String category) {
+        ArrayList<Book> filteredCart = new ArrayList<>();
+        for (Book book : cart) {
+            if((book.getCategory()).equals(condition)) {
+                filteredCart.add(book);
+            }
+        }
+        return filteredCart;
+    }
+
+    public void addToCart(Book book) {
+        cart.add(book);
+    }
+
+    public ArrayList<Book> viewCart() {
+        return cart;
+    }
+
+    public String purchase() {
+        String purchased = "";
+        return purchased;
+    }
+}
